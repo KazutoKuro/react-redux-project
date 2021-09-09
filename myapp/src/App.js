df1630883a272c41fb33529082bd620fd28ddd91
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-09 21:20:26 +0630

    vid-14: Outputting Lists

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,14 +1,44 @@
+// vid 1 to 13
+
+// import React, { Component } from 'react';
+// import Ninjas from './Ninjas';
+
+// class App extends Component{
+//    render() {
+//      return (
+//       <div className="App">
+//         <h1>My first React app!</h1>
+//         <p>Welcome</p>
+//         <Ninjas name="Ryu" age="25" belt="black " />
+//         <Ninjas name="Yoshi" age="30" belt="red " />
+//       </div>
+//      );
+//    }
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-14: Outputting Lists
+
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 
 class App extends Component{
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 40, belt: 'black', id: 1},
+      { name: 'Yoshi', age: 20, belt: 'black', id: 2},
+      { name: 'Crystal', age: 25, belt: 'black', id: 3}
+    ]
+  }
    render() {
      return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome</p>
-        <Ninjas name="Ryu" age="25" belt="black " />
-        <Ninjas name="Yoshi" age="30" belt="red " />
+        <Ninjas ninjas={ this.state.ninjas }/>
       </div>
      );
    }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,16 +1,47 @@
+// vid 1 to 13
+
+// import React, { Component } from 'react';
+
+// class Ninjas extends Component{
+//     render(){
+//         // console.log(this.props);
+//         const { name, age, belt } = this.props
+//         return(
+//             <div className="ninja">
+//             <div>Name: {this.props.name}</div>
+//             <div>Age: {this.props.age}</div>
+//             <div>Belt: {this.props.belt}</div>
+//         </div>
+//         );
+//     }
+// }
+
+// export default Ninjas;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-14: Outputting Lists
+
 import React, { Component } from 'react';
 
 class Ninjas extends Component{
     render(){
         // console.log(this.props);
-        const { name, age, belt } = this.props
+        const { ninjas} = this.props;
+        const ninjaList = ninjas.map(ninja => {
+            return (
+                <div className="ninja" key={ninja.id}>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+            )   
+        })
         return(
-            <div className="ninja">
-            <div>Name: {this.props.name}</div>
-            <div>Age: {this.props.age}</div>
-            <div>Belt: {this.props.belt}</div>
-        </div>
-        );
+            <div className="ninja-list">
+                { ninjaList }
+            </div>
+        )
     }
 }
 
