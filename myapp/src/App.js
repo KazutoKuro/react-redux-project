c91ef2d7bef8d9298009327d14af9cafdde65dd5
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-11 02:14:33 +0630

    vid-19: deleting data

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -52,6 +52,44 @@
 
 // vid-18: Function as props
 
+// import React, { Component } from 'react';
+// import Ninjas from './Ninjas';
+// import AddNinja from './AddNinja';
+
+// class App extends Component{
+//   state = {
+//     ninjas: [
+//       { name: 'Ryu', age: 40, belt: 'black', id: 1},
+//       { name: 'Yoshi', age: 20, belt: 'black', id: 2},
+//       { name: 'Crystal', age: 25, belt: 'black', id: 3}
+//     ]
+//   }
+//   addNinja = (ninja) => {
+//     ninja.id = Math.random();
+//     let ninjas = [...this.state.ninjas, ninja]
+//     this.setState({
+//       ninjas: ninjas
+//     })
+//   }
+
+//    render() {
+//      return (
+//       <div className="App">
+//         <h1>My first React app!</h1>
+//         <p>Welcome</p>
+//         <Ninjas ninjas={ this.state.ninjas }/>
+//         <AddNinja addNinja ={this.addNinja} />
+//       </div>
+//      );
+//    }
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-19: deleting data
+
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 import AddNinja from './AddNinja';
@@ -69,7 +107,16 @@ class App extends Component{
     let ninjas = [...this.state.ninjas, ninja]
     this.setState({
       ninjas: ninjas
-    })
+    });
+  }
+
+  deleteNinja = (id) => {
+    let ninjas = this.state.ninjas.filter(ninja => {
+      return ninja.id !== id // if the id is not equal to the id of current ninja,returning true n remain in the new arry
+    });
+    this.setState({
+      ninjas: ninjas
+    });
   }
 
    render() {
@@ -77,7 +124,7 @@ class App extends Component{
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome</p>
-        <Ninjas ninjas={ this.state.ninjas }/>
+        <Ninjas deleteNinja={this.deleteNinja} ninjas={ this.state.ninjas } />
         <AddNinja addNinja ={this.addNinja} />
       </div>
      );
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -76,45 +76,73 @@
 
 // vid-16: Conditional Output
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-const Ninjas = ({ninjas}) => {
-    // const ninjaList = ninjas.map(ninja => {
-    //     if(ninjas.age > 20){
-    //         return (
-    //             <div className="ninja" key={ninja.id}>
-    //                 <div>Name: {ninja.name}</div>
-    //                 <div>Age: {ninja.age}</div>
-    //                 <div>Belt: {ninja.belt}</div>
-    //             </div>
-    //         )   
-    //     } else {
-    //         return null
-    //     }
-    // })
+// const Ninjas = ({ninjas}) => {
+//     // const ninjaList = ninjas.map(ninja => {
+//     //     if(ninjas.age > 20){
+//     //         return (
+//     //             <div className="ninja" key={ninja.id}>
+//     //                 <div>Name: {ninja.name}</div>
+//     //                 <div>Age: {ninja.age}</div>
+//     //                 <div>Belt: {ninja.belt}</div>
+//     //             </div>
+//     //         )   
+//     //     } else {
+//     //         return null
+//     //     }
+//     // })
    
-    // return(
-    //     <div className="ninja-list">
-    //         { ninjaList }
-    //     </div>
-    // )
+//     // return(
+//     //     <div className="ninja-list">
+//     //         { ninjaList }
+//     //     </div>
+//     // )
     
-    return(
-        <div className="ninja-list">
-            { 
-                ninjas.map(ninja => {
-                    // condition ? () : () // true : false
-                    return ninja.age ? (
-                        <div className="ninja" key={ninja.id}>
-                            <div>Name: {ninja.name}</div>
-                            <div>Age: {ninja.age}</div>
-                            <div>Belt: {ninja.belt}</div>
-                        </div>
-                    ) : null;
-                })
-            }
-        </div>
-    )
+//     return(
+//         <div className="ninja-list">
+//             { 
+//                 ninjas.map(ninja => {
+//                     // condition ? () : () // true : false
+//                     return ninja.age ? (
+//                         <div className="ninja" key={ninja.id}>
+//                             <div>Name: {ninja.name}</div>
+//                             <div>Age: {ninja.age}</div>
+//                             <div>Belt: {ninja.belt}</div>
+//                         </div>
+//                     ) : null;
+//                 })
+//             }
+//         </div>
+//     )
+// }
+
+// export default Ninjas;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-19-deleting data
+
+import React from 'react'
+
+const Ninjas = ({ninjas, deleteNinja}) => {
+  return (
+    <div className="ninja-list">
+      { 
+        ninjas.map(ninja => {
+          return (
+            <div className="ninja" key={ninja.id}>
+              <div>Name: { ninja.name }</div>
+              <div>Age: { ninja.age }</div>
+              <div>Belt: { ninja.belt }</div>
+              <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
+              <hr />
+            </div>
+          )
+        })
+      }
+    </div>
+  );
 }
 
-export default Ninjas;
+export default Ninjas
