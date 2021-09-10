7a5ad34f7ecd5246b42b3024e249598c188a4f8c
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-11 01:43:15 +0630

    vid-18: functions as Props

--- myapp/src/AddNinja.js
+++ myapp/src/AddNinja.js
@@ -1,5 +1,47 @@
 // vid-17: forms revisited
 
+// import React, { Component } from 'react'
+// import Ninjas from './Ninjas'
+
+// class AddNinja extends Component {
+//     state = {
+//         name: null,
+//         age: null,
+//         belt: null
+//     }
+//     handleChange = (e) => {
+//         this.setState({
+//             [e.target.id] : e.target.value
+//         })
+//     }
+//     handleSubmit = (e) => {
+//         e.preventDefault();
+//         console.log(this.state);
+
+//     }
+//     render(){
+//         return (
+//         <div>
+//                 <form onSubmit={this.handleSubmit}>
+//                     <label htmlFor="name">Name: </label>
+//                     <input type="text" id="name" onChange={this.handleChange} />
+//                     <label htmlFor="name">Age: </label>
+//                     <input type="text" id="age" onChange={this.handleChange} />
+//                     <label htmlFor="name">Belt: </label>
+//                     <input type="text" id="bel;t" onChange={this.handleChange} />
+//                     <button>Submit</button>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
+
+// export default AddNinja;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-18: Functions as props
+
 import React, { Component } from 'react'
 import Ninjas from './Ninjas'
 
@@ -16,8 +58,7 @@ class AddNinja extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
-
+        this.props.addNinja(this.state);
     }
     render(){
         return (
@@ -37,4 +78,5 @@ class AddNinja extends Component {
 }
 
 export default AddNinja;
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////
--- myapp/src/App.js
+++ myapp/src/App.js
@@ -22,6 +22,36 @@
 
 // vid-14: Outputting Lists
 
+// import React, { Component } from 'react';
+// import Ninjas from './Ninjas';
+// import AddNinja from './AddNinja';
+
+// class App extends Component{
+//   state = {
+//     ninjas: [
+//       { name: 'Ryu', age: 40, belt: 'black', id: 1},
+//       { name: 'Yoshi', age: 20, belt: 'black', id: 2},
+//       { name: 'Crystal', age: 25, belt: 'black', id: 3}
+//     ]
+//   }
+//    render() {
+//      return (
+//       <div className="App">
+//         <h1>My first React app!</h1>
+//         <p>Welcome</p>
+//         <Ninjas ninjas={ this.state.ninjas }/>
+//         <AddNinja />
+//       </div>
+//      );
+//    }
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-18: Function as props
+
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 import AddNinja from './AddNinja';
@@ -34,13 +64,21 @@ class App extends Component{
       { name: 'Crystal', age: 25, belt: 'black', id: 3}
     ]
   }
+  addNinja = (ninja) => {
+    ninja.id = Math.random();
+    let ninjas = [...this.state.ninjas, ninja]
+    this.setState({
+      ninjas: ninjas
+    })
+  }
+
    render() {
      return (
       <div className="App">
         <h1>My first React app!</h1>
         <p>Welcome</p>
         <Ninjas ninjas={ this.state.ninjas }/>
-        <AddNinja />
+        <AddNinja addNinja ={this.addNinja} />
       </div>
      );
    }
