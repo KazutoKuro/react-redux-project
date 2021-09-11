3aed791312ab4d5eff5c03c2155b27d7c48af875
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-11 23:15:58 +0630

    vid-20: css files

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -123,7 +123,36 @@
 
 // vid-19-deleting data
 
-import React from 'react'
+// import React from 'react';
+
+// const Ninjas = ({ninjas, deleteNinja}) => {
+//   return (
+//     <div className="ninja-list">
+//       { 
+//         ninjas.map(ninja => {
+//           return (
+//             <div className="ninja" key={ninja.id}>
+//               <div>Name: { ninja.name }</div>
+//               <div>Age: { ninja.age }</div>
+//               <div>Belt: { ninja.belt }</div>
+//               <button onClick={() => {deleteNinja(ninja.id)}}>Delete ninja</button>
+//               <hr />
+//             </div>
+//           )
+//         })
+//       }
+//     </div>
+//   );
+// }
+
+// export default Ninjas
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-21: css files
+
+import React from 'react';
+import './Ninjas.css'
 
 const Ninjas = ({ninjas, deleteNinja}) => {
   return (
