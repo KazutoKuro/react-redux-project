adf388ecdc6ef7a603a6907a34783a3ef6481fa3
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-13 23:54:27 +0630

    vid-22: lifecycle method

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -90,6 +90,53 @@
 
 // vid-19: deleting data
 
+// import React, { Component } from 'react';
+// import Ninjas from './Ninjas';
+// import AddNinja from './AddNinja';
+
+// class App extends Component{
+//   state = {
+//     ninjas: [
+//       { name: 'Ryu', age: 40, belt: 'black', id: 1},
+//       { name: 'Yoshi', age: 20, belt: 'black', id: 2},
+//       { name: 'Crystal', age: 25, belt: 'black', id: 3}
+//     ]
+//   }
+//   addNinja = (ninja) => {
+//     ninja.id = Math.random();
+//     let ninjas = [...this.state.ninjas, ninja]
+//     this.setState({
+//       ninjas: ninjas
+//     });
+//   }
+
+//   deleteNinja = (id) => {
+//     let ninjas = this.state.ninjas.filter(ninja => {
+//       return ninja.id !== id // if the id is not equal to the id of current ninja,returning true n remain in the new arry
+//     });
+//     this.setState({
+//       ninjas: ninjas
+//     });
+//   }
+
+//    render() {
+//      return (
+//       <div className="App">
+//         <h1>My first React app!</h1>
+//         <p>Welcome</p>
+//         <Ninjas deleteNinja={this.deleteNinja} ninjas={ this.state.ninjas } />
+//         <AddNinja addNinja ={this.addNinja} />
+//       </div>
+//      );
+//    }
+// }
+
+// export default App;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// vid-22: Lifecycle Methods
+
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 import AddNinja from './AddNinja';
@@ -117,6 +164,15 @@ class App extends Component{
     this.setState({
       ninjas: ninjas
     });
+  }
+
+  componentDidMount(){
+    console.log('component mounted')
+  }
+  
+  componentDidUpdate(prevProps, prevState){
+    console.log('component updated');
+    console.log(prevProps, prevState);
   }
 
    render() {
