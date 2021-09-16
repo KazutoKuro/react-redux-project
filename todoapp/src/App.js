38d270c0b9649e7b2c3a96f7a5a40771891a8728
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-17 02:57:19 +0630

    vid-24: todos part 2

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,8 @@
+//vid-23, 24 (Todos part 1 and 2)
+
 import React, { Component } from 'react';
 import Todos from './Todos';
+import AddTodo from './AddTodo';
 
 class App extends Component{
   state = {
@@ -18,14 +21,24 @@ class App extends Component{
       todos
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random();
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    })
+  }
   render() {
     return(
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos = {this.state.todos} deleteTodo={this.deleteTodo} />
+        <AddTodo addTodo={this.addTodo} />
       </div>
     );
   }
 }
 
 export default App;
+
+//////////////////////////////////////////////////////////////////////
