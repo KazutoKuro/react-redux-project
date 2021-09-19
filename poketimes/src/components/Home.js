c04f248a0f6790402576d4bff9977b11dc579351
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-20 01:40:47 +0630

    vid-29: Using Axios

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const Home = () => {
-    return(
-        <div className="container">
-            <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Totam quis ad aperiam aliquam perferendis necessitatibus dignissimos, at voluptates eligendi dolorum molestias maxime ipsum eum dicta vel nisi optio ex perspiciatis.</p>
-        </div>
-    )
+class Home extends Component {
+    state = {
+        posts: []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    posts: res.data.slice(0,10)
+                })
+            })
+    }
+    
+    render() {
+        const { posts } = this.state;
+        const postList = posts.length ? (
+            posts.map(post => {
+                return (
+                    <div className="post card" key={post.id}>
+                        <div className="card-content">
+                            <span className="card-title">{post.title}</span>
+                            <p>{post.body}</p>
+                        </div>
+                    </div>
+                )
+            })
+        ) : (
+            <div className="center">No posts yet</div>
+        )
+        return(
+            <div className="container">
+                <h4 className="center">Home</h4>
+                {postList}
+            </div>
+        )
+    }
 }
 
 export default Home;
--- poketimes/src/components/Navbar.js
+++ poketimes/src/components/Navbar.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Link, NavLink, withRouter } from 'react-router-dom';
 
 const Navbar = (props) => {
-    setTimeout(() => {
-        props.history.push('/about')
-    }, 2000);
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000);
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
@@ -20,4 +20,5 @@ const Navbar = (props) => {
     )
 }
 
-export default withRouter(Navbar);
+// export default withRouter(Navbar);
+export default Navbar;
