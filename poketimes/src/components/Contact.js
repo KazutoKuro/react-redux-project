4a2dabd591398dce68c00c6509deca716411227c
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-20 01:03:03 +0630

    vid-27: Programmatic Redirects

--- poketimes/src/components/Contact.js
+++ poketimes/src/components/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
-const Contact = () => {
+const Contact = (props) => {
+    setTimeout(() => {
+        props.history.push('/about')
+    }, 2000);
     return(
         <div className="container">
             <h4 className="center">Contact</h4>
