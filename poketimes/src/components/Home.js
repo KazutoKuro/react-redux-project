917f61aa9b75ddb262a8d1e248f84ffedf38f69a
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-22 22:30:46 +0630

    vid-40:Mapping State to Props

--- poketimes/src/components/Home.js
+++ poketimes/src/components/Home.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react'
-import axios from 'axios'
+// import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Pokeball from '../pokeball.png'
+import { connect } from 'react-redux'
 
 class Home extends Component {
-  state = {
-    posts: []
-  }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts/')
-      .then(res => {
-        console.log(res);
-        this.setState({
-          posts: res.data.slice(0,10)
-        });
-      })
-  }
+  // state = {
+  //   posts: []
+  // }
+  // componentDidMount(){
+  //   axios.get('https://jsonplaceholder.typicode.com/posts/')
+  //     .then(res => {
+  //       console.log(res);
+  //       this.setState({
+  //         posts: res.data.slice(0,10)
+  //       });
+  //     })
+  // }
   render(){
-    const { posts } = this.state
+    console.log(this.props)
+    // const { posts } = this.state;
+    const { posts } = this.props;
     const postList = posts.length ? (
       posts.map(post => {
         return (
@@ -47,4 +50,10 @@ class Home extends Component {
   }
 }
 
-export default Home
+const mapStateToProps = (state) => {
+  return{
+    posts: state.posts
+  }
+}
+
+export default connect(mapStateToProps)(Home)
