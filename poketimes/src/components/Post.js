501ce06deb7eddee004559d78e0e93409e8726ec
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-23 21:05:48 +0630

    vid-43: Action Creates & fixing vid-42: map dispatch to props

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 // import axios from 'axios'
 import { connect } from 'react-redux'
+import {deletePost} from '../actions/postActions'
 
 class Post extends Component {
   // state = {
@@ -57,7 +58,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) =>{
   return{
-    deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id })}
+    // deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id })}
+    deletePost: (id) => { dispatch(deletePost(id))}
   }
 }
 
