960ace61ee614afa6dbb5537a1772174e8d1e5c6
KauztoKuro <ikkikurogane70@gmail.com>
2021-09-23 20:31:04 +0630

    vid-43: Map Dispatch to Props

--- poketimes/src/components/Post.js
+++ poketimes/src/components/Post.js
@@ -15,8 +15,15 @@ class Post extends Component {
   //     })
   //   })
   // }
+
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+  }
+
   render() {
-    
+    console.log(this.props);
+
     // const post = this.state.post ? (
     const post = this.props.post ? (
       <div className="post">
@@ -24,6 +31,11 @@ class Post extends Component {
         <h4 className="center">{this.props.post.title}</h4>
         {/* <p>{this.state.post.body}</p> */}
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn grey" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -43,4 +55,11 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 
-export default connect(mapStateToProps)(Post)
+const mapDispatchToProps = (dispatch) =>{
+  return{
+    deletePost: (id) => { dispatch({type: 'DELETE_POST', id: id })}
+  }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post)
--- poketimes/src/reducers/rootReducer.js
+++ poketimes/src/reducers/rootReducer.js
@@ -1,4 +1,5 @@
 const initState ={
+    users: [],
     posts: [
         {id: '1', title: 'Squirtle Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'},
         {id: '2', title: 'Charmander Laid an Egg', body: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur voluptate laborum perferendis, enim repellendus ipsam sunt autem at odit dolorum, voluptatum suscipit iste harum cum magni itaque animi laudantium fugiat'},
@@ -7,6 +8,15 @@ const initState ={
 }
 
 const rootReducer = (state = initState, action) => {
+    if(action.type === 'DELETE_POST') {
+        let newPosts = state.posts.filter(post => {
+            return action.id !== post.id
+        });
+        return {
+            ...state,
+            posts: newPosts
+        }
+    }
     return state;
 }
 
